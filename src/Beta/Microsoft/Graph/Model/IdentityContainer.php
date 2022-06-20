4eef8cb7155f8a2cc27335f5fa66dbabca981aa8
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2022-06-20 15:08:13 +0000

    Update generated files with build 77831 [skip ci]

--- src/Beta/Microsoft/Graph/Model/IdentityContainer.php
+++ src/Beta/Microsoft/Graph/Model/IdentityContainer.php
@@ -331,7 +331,8 @@ class IdentityContainer implements \JsonSerializable
     *
     * @return array The list of properties
     */
-    public function jsonSerialize(): array
+    #[\ReturnTypeWillChange]
+    public function jsonSerialize()
     {
         $serializableProperties = $this->getProperties();
         foreach ($serializableProperties as $property => $val) {
